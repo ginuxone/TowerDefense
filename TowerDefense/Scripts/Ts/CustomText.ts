97630f5e2e669e9844162edb5679606b7b5f7450
GinoAlessandro Milla <ginomilla@outlook.com>
2016-05-14 00:44:35 +0200

    Created Custom Text Class

---
+++ TowerDefense/Scripts/Ts/CustomText.ts
@@ -0,0 +1,16 @@
+﻿module TowerDefense {
+    export class CustomText extends Phaser.Text {
+        text: string;
+        game: Phaser.Game;
+        constructor(g: Game, x: number, y: number, t: string) {
+            super(g, x, y, t, { font: "65px Arial", fill: "#ff0044", align: "center" });
+            this.game = g;
+            this.text = t;
+        }
+
+        create() {
+            var textFill = new CustomText(this.game, this.game.world.centerX, this.game.world.centerY, this.text);
+            this.game.add.existing(textFill);
+        };
+    }
+}
